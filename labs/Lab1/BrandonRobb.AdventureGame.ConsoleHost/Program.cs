b0822eac3f829fbe2943afdca201a26318a3f542
Brandon Robb <brandon.robb@my.tccd.edu>
2021-09-01 16:44:51 -0500

    Adventure Game Initial

---
+++ labs/Lab1/BrandonRobb.AdventureGame.ConsoleHost/Program.cs
@@ -0,0 +1,18 @@
+﻿/*
+ * file header
+ * Brandon Robb
+ * ITSE 1430 Fall 2021
+ */
+
+using System;
+
+namespace BrandonRobb.AdventureGame.ConsoleHost
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
