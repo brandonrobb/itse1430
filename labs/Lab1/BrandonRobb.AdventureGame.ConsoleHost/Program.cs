f82cb59c6101fd26b9dad8996e792ec819c2c6ba
Brandon Robb <brandon.robb@my.tccd.edu>
2021-09-01 19:00:38 -0500

    saving intro

--- labs/Lab1/BrandonRobb.AdventureGame.ConsoleHost/Program.cs
+++ labs/Lab1/BrandonRobb.AdventureGame.ConsoleHost/Program.cs
@@ -10,13 +10,29 @@ namespace BrandonRobb.AdventureGame.ConsoleHost
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main ( string[] args )
         {
-            Console.WriteLine(" You have awoken from your long nights rest, suprisingly to find yourself stranded ");
-            Console.WriteLine("on a sinking cruise ship. Search the rooms of the ship to find materials to patch the holes located ");
-            Console.WriteLine("in 3 different rooms in the ship. Then after, board the auxilary life boat to escape !");
+            
+                Console.WriteLine(" You have awoken from your long nights rest, suprisingly to find yourself stranded ");
+                Console.WriteLine("on a sinking cruise ship. Search the rooms of the ship to find  ");
+                Console.WriteLine("materials to patch the holes in the ship located in 3 different rooms. ");
+                Console.WriteLine("Then after, you will be able to board the auxilary life boat to escape! ");
 
+                Console.ReadLine();
+                Console.Clear();
+
+            
+            
+           /* Console.WriteLine("Type YES To Begin or Type NO to Exit");
+            
+          
+             * string enterGame = Console.ReadLine();
+            
+            if enterGame = ("YES")
+
+            */
             Console.ReadLine();
+
         }
     }
 }
