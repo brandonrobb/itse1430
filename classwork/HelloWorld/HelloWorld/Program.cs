c069d551dca54674d5aa17ce543e3f6f439488a1
Brandon Robb <brandon.robb@my.tccd.edu>
2021-09-13 21:26:00 -0500

    updade

--- classwork/HelloWorld/HelloWorld/Program.cs
+++ classwork/HelloWorld/HelloWorld/Program.cs
@@ -12,24 +12,138 @@ namespace HelloWorld
         static void Main(string[] args)
         {
             static void DemoLogicalOperators()
+                static void DemoRelationalOperators()
             {
 
-                //Logical And - true if both operands are true
-                //Logical Or - true if either operand is true
-                // X    Y   &&  ||
-                // -----------------
-                // F    F   F   F
-                //F     T   F   T
-                //T     F   F   T
-                //T     T   T   T
+                int x = 10, y = 20;
 
+                bool isLessThan = x < y;
+                bool isLessThanorEqualTo = x <=y;
 
-                //NOT 
-                //  F   !F =    T
-                //  T   !T =    F
+                bool isGreaterThan = x > y;
+                bool isGreaterThanOrEqualTo = x>= y;
+
+                bool isEqual = x== y;
+                bool isNotEqual = x != y;
             }
 
+            static void DemoCombinationOperators()
+            {
+                //Works for more than just arithmetic
+                int x = 10;
+                x += 10; // x= x+ 10
+                x -=20; // x= - 20
+                x += 10; // x= x+ 10
+                x += 10; // x= x+ 10
+            }
+
+            // Prefix increment
+            // 1. Take current value of x and increment by 1
+            // @. Store new value back in X
+            // #. Expression value is current value of x
+            y = ++x; //x = 11, y = 11
+
+            // Prefix increment
+            // 1. Take current value of x and increment by 1
+            // @. Store new value back in X
+            // #. Expression value is current value of x
+            y = --x; //x = 10, y = 10
+
+            // Postfix increment
+            // 1. Take current value of x and increment by 1
+            // @. Store new value back in X
+            // #. Expression value is current value of x
+            y = x++; //x = 11, y = 10
+
+            // Postfix increment
+            // 1. Take current value of x and increment by 1
+            // @. Store new value back in X
+            // #. Expression value is current value of x
+            y = x--; //x = 10, y = 11
+
+
+            //Logical And - true if both operands are true
+            //Logical Or - true if either operand is true
+            // X    Y   &&  ||
+            // -----------------
+            // F    F   F   F
+            //F     T   F   T
+            //T     F   F   T
+            //T     T   T   T
+
+
+            //NOT 
+            //  F   !F =    T
+            //  T   !T =    F
+        }
+        static void DemoAssignmentOperator()
+        {
+            int x;
+            int y;
+            int z;
+
+            x = 10;
+            y = 10;
+            z = 10;
+
+            //left associative (E1 op E2) => E1, E2, op
+            // right associative operator (E1 op E2) => E2, e1, op
+            x=y=z=20;// x = (y=(z=20))
+        }
            
+        static void Conditional Operator ()
+        {
+            int grade = 70;
+            string passStatus;
+            if (grade < 60)
+                passStatus= "Not Passing";
+            else
+                passStatus = "Passing";
+            //Terninary - operands
+            //E(bool) ? E(True) : E(false)
+           string passStatus2 =(grade<60) ? "Not Passing" : "Passing";
+
+            static void DemoStrings()
+            {
+                string firstName = "Bob";
+                //Relationals work, case sensitive
+                bool isBob = firstName =="Bob";
+
+                //String literals
+                string literal1 = "Hello World";
+
+                //Escape sequence - \?, only works in string literals
+                // =>\n  newline
+                // =>\t  horizontal tab
+                // \\  slash
+                // \"  double quote
+
+
+                string filePath = "C:\\windows\\system32\\tasks";
+                string verbatimFilePath = @"C:\windows\systems32\tasks";
+
+
+
+                string quoteString = "Hello\"Bob\"";
+                literal1 = "Hello\nWorld";
+
+                //String length Length => how many characters
+                int length = literal1.Length;
+
+                //Empty string
+                string emptyString = ""; // >length = 0
+                string emptyString2 = String.Empty; // Lenghth =0
+                bool areEmptyStringsEqual = String.Empty ==""; //True
+
+                // null != empty string
+                string nullString = null;
+                bool areEqualNull
+
+            }
+                
+
+
+        }
         }
     }
 }
--- classwork/HelloWorld/MovieLibrary.ConsoleHost/Program.cs
+++ classwork/HelloWorld/MovieLibrary.ConsoleHost/Program.cs
@@ -15,33 +15,47 @@ namespace MovieLibrary.ConsoleHost
             do
             {
                 char choice = GetInput();
-                if (choice == 'Q')
-                    done = HandleQuit();
-                else if (choice == 'A')
-                    AddMovie();
-                else if (choice == 'V')
-                    ViewMovie();
-                else if (choice == 'D')
-                    DeleteMovie();
-                else
-                    Console.WriteLine("Unknown option");
+
+                //if (choice == 'Q' || choice == 'q')
+                //done = HandleQuit();
+                //if (choice == 'Q')
+                //    done = HandleQuit();
+                //else if (choice == 'A')
+                //    AddMovie();
+                //else if (choice == 'V')
+                //    ViewMovie();
+                //else if (choice == 'D')
+                //    DeleteMovie();
+                //else
+                //    Console.WriteLine("Unknown option");
+                switch (choice)
+                    {
+                    case 'Q':
+                    {
+                        done = HandleQuit(); break;
+                    };
+                    case 'A': AddMovie(); break;
+                    case 'V': ViewMovie(); break;
+                    case 'D': DeleteMovie(); break;
+                    default: DisplayError("Unknown Option"); break;
+                };
             } while (!done);
-            //TODO: handle additional stuff            
         }
 
         private static void DeleteMovie ()
         {
-            if (!ReadBoolean("Are you sure(Y/N)?"))
+            //Confirm
+            if (!ReadBoolean("Are you sure (Y/N)? "))
                 return;
-            //TODO delete movie
+
+            //TODO: delete movie
             Console.WriteLine("Not implemented");
         }
-      static string title;
-        static string description;
 
+        static string title;
+        static string description;
         static int runLength;
         static int releaseYear;
-
         static double reviewRating;
         static string rating;
         static bool isClassic;
@@ -55,12 +69,15 @@ namespace MovieLibrary.ConsoleHost
             runLength = ReadInt32("Enter run length (in minutes): ", 0); // >= 0
             releaseYear = ReadInt32("Enter the release year (min 1900): ", 1900); //1900+
 
-            //reviewRating;  //Optional, 0.0 to 5.0
+            //double reviewRating;  //Optional, 0.0 to 5.0
             rating = ReadString("Enter the MPAA rating: ", false); //MPAA (not enforced)
-            isClassic = ReadBoolean("Is this a classic (Y/N)?");       //Optional
+            isClassic = ReadBoolean("Is this a classic (Y/N)? ");       //Optional
         }
+
         static void ViewMovie ()
         {
+            //TODO: What if they haven't added one yet?
+            //TODO: Formatting
             Console.WriteLine(title);
             Console.WriteLine(releaseYear);
             Console.WriteLine(runLength);
@@ -69,76 +86,77 @@ namespace MovieLibrary.ConsoleHost
             Console.WriteLine(description);
         }
 
+        //Handles quit logic
         private static bool HandleQuit ()
         {
-            //return ReadBoolean("Are you sure you want to quit (Y/N)? ");
-
             //Display a confirmation
             if (ReadBoolean("Are you sure you want to quit (Y/N)? "))
                 return true;
 
             return false;
+
+            //Shortcut of below code, preferred
+            //return ReadBoolean("Are you sure you want to quit (Y/N)? ");
         }
-
-
-
 
         private static bool ReadBoolean ( string message )
         {
             Console.Write(message);
+
             do
             {
                 ConsoleKeyInfo input = Console.ReadKey(true);
                 if (input.Key == ConsoleKey.Y)
                     return true;
-                else if (input.Key == ConsoleKey.N);
-                    //TODO: Input validation
-                    //return false;
+                else if (input.Key == ConsoleKey.N)
+                    return false;
             } while (true);
+
+            //Not needed anymore
+            //return false;
         }
 
         static int ReadInt32 ( string message, int minimumValue )
         {
             Console.Write(message);
 
+            //Validate input
             do
             {
+                //var x;
+                //string input = Console.ReadLine();
+                var input = Console.ReadLine();
 
-
-                string input = Console.ReadLine();
-                //var input = Console.ReadLine();
-
-                //TODO: Input validation
                 //int result = Int32.Parse(input); //Crashes program, not good for input
-                //
-                //
-
                 //int result;
 
-                //
-                //
+                //if string parsed AND result at least min value
+                //if (Int32.TryParse(input, out result))
+                //    if (result >= minimumValue)
                 if (Int32.TryParse(input, out var result) && result >= minimumValue)
                     return result;
 
-                DisplayError("The value must be an integral value >=" + minimumValue);
-            }while (true);
+                DisplayError("The value must be an integral value >= " + minimumValue);
+            } while (true);
 
-            //return -1
+            //return -1;
         }
 
         static string ReadString ( string message, bool required )
         {
             Console.Write(message);
+
             do
             {
-                //TODO: Input validation - required, normalize
                 string input = Console.ReadLine();
+
+                //TODO: required, normalize
+
                 return input;
             } while (true);
-
-           
         }
-        static void DisplayError (string message)
+
+        static void DisplayError ( string message )
         {
             Console.ForegroundColor = ConsoleColor.Red;
             Console.WriteLine(message);
@@ -150,23 +168,38 @@ namespace MovieLibrary.ConsoleHost
             Console.WriteLine("Movie Library");
             Console.WriteLine("---------------");
 
-            while (true) {
-                Console.WriteLine("A) dd");
-                Console.WriteLine("V)iew");
-                Console.WriteLine("D) elete");
-                Console.WriteLine("Q) uit");
+            Console.WriteLine("A) dd");
+            Console.WriteLine("V) iew");
+            Console.WriteLine("D) elete");
+            Console.WriteLine("Q) uit");
 
-                //TODO: Input validation
+            while (true)
+            {
                 //Get input
                 string input = Console.ReadLine();
-                if (input == "Q")
+                /*if (input == "Q")
                     return 'Q';
                 else if (input == "A")
                     return 'A';
                 else if (input == "V")
                     return 'V';
                 else if (input == "D")
-                    return 'D';
+                    return 'D'; */
+                switch (input)
+                {   //No fallthrough must  
+                    // must end in break or return
+                    case "q":
+                    case "Q": return 'Q';
+                    case "a":
+                    case "A": return 'A';
+                    case "v":
+                    case "V": return 'V';
+                    case "d":
+                    case "D": return 'D';
+                    
+                    //default:; 
+                };
+
                 DisplayError("Invalid input");
             };
 
